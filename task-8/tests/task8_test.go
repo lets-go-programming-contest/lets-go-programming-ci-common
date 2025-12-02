fc55a077d5fdbca49d36ecaec0afa1a24fe127e8
Денис Королев <d.korolev@automacon.ru>
2025-12-02 20:30:19 +0300

    Add tests for task-8
    Fix tests for task-8
    
    Fix tests for task-8
    
    Fix tests for task-8
    
    Fix tests for task-8
    
    Fix tests for task-8
    
    Fix tests for task-8
    
    Revert "Fix tests for task-8"
    
    This reverts commit 6587b2d5f3e7507a138b41677c25b81dda2d263b.
    
    Fix tests for task-8
    
    Fix tests for task-8
    
    Fix tests for task-8

---
+++ task-8/tests/task8_test.go
@@ -0,0 +1,68 @@
+package ci
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testTimeout = time.Second
+
+type internalTestError struct {
+	reason error
+}
+
+func (e *internalTestError) Error() string {
+	return "internal ci test error: " + e.reason.Error()
+}
+
+func (e *internalTestError) Unwrap() error {
+	return e.reason
+}
+
+var casesTestService = []struct {
+	name     string
+	tag      string
+	expected string
+}{
+	{
+		name:     "test_case_1",
+		tag:      "",
+		expected: "dev debug",
+	},
+}
+
+func TestMyProgram(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range casesTestService {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			bin := filepath.Join(os.Getenv("BUILD_BIN"), "service")
+
+			ctx, closeFn := context.WithTimeout(context.TODO(), testTimeout)
+			defer closeFn()
+
+			cmd := exec.CommandContext(ctx, bin)
+
+			stdErrBuffer := bytes.NewBuffer([]byte{})
+			cmd.Stderr = stdErrBuffer
+
+			output, err := cmd.Output()
+			require.NoError(t, err, internalTestError{
+				reason: err,
+			})
+
+			require.Equal(t, tt.expected, string(output), "Output is not correct. Must be \"environment log_level\".")
+		})
+	}
+}
